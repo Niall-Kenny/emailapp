1be509e4cf3876b42ad42dc1906b67f0526de6bb
Niall Kenny <niall.s.kenny@gmail.com>
2019-09-22 15:56:52 +0100

    1st commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,22 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String alternateEmail;
+    private int mailBoxCapacity;
+
+    // Constructor to receive lastname and firstname
+
+    //Ask for department
+
+    // Generate random password
+
+    // set the mailbox capacity
+
+    // set the alternate email
+
+    // change the password
+}
